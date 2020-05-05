717da94065945d2a9124c03113da6b0d4dfb792f
Neto Deolino <netodeolino@gmail.com>
2020-05-04 22:10:47 -0300

    stated graphql tests

--- src/test/java/com/br/study/GraphqlApplicationTests.java
+++ src/test/java/com/br/study/GraphqlApplicationTests.java
@@ -1,13 +1,13 @@
-package com.br.study.graphql;
+package com.br.study;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GraphqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
