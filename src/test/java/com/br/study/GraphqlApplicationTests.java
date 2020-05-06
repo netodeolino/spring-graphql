02afe82846a3732452ca266dea05631f8270610e
Neto Deolino <netodeolino@gmail.com>
2020-05-06 20:51:54 -0300

    added dockerfile

--- src/test/java/com/br/study/GraphqlApplicationTests.java
+++ src/test/java/com/br/study/GraphqlApplicationTests.java
@@ -3,11 +3,11 @@ package com.br.study;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GraphqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
